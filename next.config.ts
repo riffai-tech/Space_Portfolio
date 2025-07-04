bdc0f606e8fb7cc60d4bd82b19dd7865ba59f069
riffai-tech <riffai.tech@gmail.com>
2025-07-04 14:37:51 +0700

    Updated next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["images.pexels.com"], 
-    
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        port: '',
+        pathname: '/photos/**',
+      },
+    ],
   },
-
-
 };
 
 export default nextConfig;
