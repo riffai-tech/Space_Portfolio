74d855c525a5f151743130d361b10895bc7df983
riffai-tech <riffai.tech@gmail.com>
2025-07-04 14:31:55 +0700

    Updated next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["i.ibb.co","images.pexels.com"], 
+    domains: ["images.pexels.com"], 
     
   },
 
